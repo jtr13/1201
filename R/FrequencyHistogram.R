53cbf1094be695defab6bb0ea2a7d3c5b41ad660
jtr13 <jtr13@columbia.edu>
2017-09-17 10:29:10 -0400

    move

--- R/FrequencyHistogram.R
+++
@@ -1,9 +0,0 @@
-# frequency histograms
-
-prices <- c(379, 425, 450, 450, 499, 529, 535, 535, 545, 599, 665,
-            675, 699, 699, 725, 725, 745, 799)
-
-hist(prices)
-
-hist(prices, breaks = c(300, 400, 500, 600, 700, 800),
-     col = "lightblue")
